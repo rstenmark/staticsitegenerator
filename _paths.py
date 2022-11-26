913d37a94bbb26ce6eec4d9c348830f793bb05c8
Ryan Stenmark <rstenmark>
2022-11-25 20:04:35 -0700

    Move path magic strings to own file

---
+++ _paths.py
@@ -0,0 +1,11 @@
+# TODO: Use Paths instead of magic strings
+# https://docs.python.org/3/library/pathlib.html
+BUILD_DIR = "/home/rs/staticsitegenerator/"
+ROOT_DIR = "www/"
+BLOG_DIR = ROOT_DIR + "blog/"
+METADATA = "/article.json"
+MOTD = "motd.html"
+TEMPLATE = "template.html"
+INDEX = "index.html"
+CONTENT_SRC = "/article.md"
+CONTENT_OUT = "/article.html"
